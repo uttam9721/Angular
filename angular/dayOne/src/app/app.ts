0db06683610a0ba9c8b882eb5be60edcac0c67e0
uttam9721 <uttammaurya377@gmail.com>
2026-03-26 00:31:52 +0530

    Learning .........

--- angular/dayOne/src/app/app.ts
+++ angular/dayOne/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[Login],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('dayOne');
+  name= "Angular Tutorial"
+  age=22
+  isAdmin=true;
+  count=5
+  
+
 }
